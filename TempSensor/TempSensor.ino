5a73b28d92f8e4221835b7d331ade30d9ebeec19
sabpog <sfp11@duke.edu>
2019-03-05 12:22:08 -0500

    Add files via upload

--- TempSensor/TempSensor.ino
+++ TempSensor/TempSensor.ino
@@ -1,48 +1,36 @@
 //Variables
 int tempPin = A0; //input the analog pin that the TMP36's Vout is connected to
 
-float sinVal;
-int toneVal; 
-
-
 void setup() {
   pinMode(8, OUTPUT); //buzzer output pin
   Serial.begin(9600); //initiate serial connection with computer
 }
 
-
 void loop() {
-  int rawTempVal = analogRead(tempPin); //saving the value received from the TMP36
+   int rawTempVal = analogRead(tempPin); //saving the value received from the TMP36
   float voltage = rawTempVal * 5.0; //converts the sensor value to voltage (for 5V)
   voltage /= 1024.0;
-
-  Serial.print(voltage); Serial.println("volts"); //printing out the voltage for visualization, can be removed later
+  
 
   float temperatureC = (voltage-0.5)*100; //voltage to temperature, first convert from 10 mv/degree with 500 mV offset
 
-  Serial.print(temperatureC); Serial.println("degrees C");
-
-  if(temperatureC>8){ //if temp rises above 8 degrees C
-    for(int x=0; x<180; x++){ 
-      sinVal=(sin(x*(3.1412/180))); //convert sin function from degrees to radians
-      toneVal = 2000+(int(sinVal*1000)); //generating a sound frequency
-
-      tone(8, toneVal); //configuring buzzer to pin 8
-      delay(2);
+   
+  Serial.println(temperatureC); Serial.println("degrees C");
+   
+   if(temperatureC>8){ //if temp rises above 8 degrees C
+   
+     tone(8, 2000, 50); //configuring buzzer to pin 8
+     delay(5000);
     }
 
-  }
   else if(temperatureC<2){
-    for(int x=0; x<180; x++){ 
-      sinVal=(sin(x*(3.1412/180))); //convert sin function from degrees to radians
-      toneVal = 2000+(int(sinVal*1000)); //generating a sound frequency
 
-      tone(8, toneVal); //configuring buzzer to pin 8
-      delay(2);
+      tone(8, 2000, 50); //configuring buzzer to pin 8
+      delay(5000);
     }
-  }
+ 
   else{
-    noTone(8);
+   noTone(8);
   }
     
 }
